dedc04aa54f59fc86b918999c2da362af1784533
Artem Kuznetsov <artemkuz1083@gmail.com>
2024-09-26 23:57:19 +0500

    feat: add Multiplication and update Program.cs

---
+++ AlgorytmsLibrary/Multiplication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorytmsLibrary
+{
+    internal class Multiplication : IResercheable
+    {
+        public Multiplication(int size, string name) : base(size, name)
+        {
+
+        }
+
+
+        public override void Run(int[] array, int value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= array[i];
+            }
+        }
+
+    }
+}
--- AlgorytmsLibrary/Program.cs
+++ AlgorytmsLibrary/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,18 +24,66 @@ namespace AlgorytmsLibrary
                 new QuickSort(12000, "QuickSort")
             };
 
+            foreach (IResercheable algorythm in algorythmList)
+            {
+                Export(algorythm);
+            }
         }
     }
 
     class Tools
     {
         // Таймер
+        public static long Timer(int[] array, IResercheable algorythm)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            algorythm.Run(array);
+            stopwatch.Stop();
+
+            return stopwatch.ElapsedMilliseconds;
+        }
 
 
         // Генерация массива
+        public static int[] GenerateArray(int size)
+        {
+            int[] array = new int[size];
 
-    
-    
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(0, 1000000);
+            }
+            return array;
+        }
+
+        //Запуск массива и экспорт данных(кол-во данных и время затраченного времени) для wpf
+        public static List<(int, long)> Export(IResercheable algorythm)
+        {
+            List<(int, long)> results = new List<(int, long)>();
+
+            // TestArray = GenerateArray(size);
+            foreach (int dimension in algorythm.TestArray)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    results.Add((dimension,
+                    Timer(GenerateArray(dimension), algorythm)));
+                }
+            }
+
+            List<(int, long)> res = new List<(int, long)>();
+            for (int i = 1; i <= algorythm.TestArray.Length; i++)
+            {
+                var average = results.Where(x => x.Item1 == i).Average(x => x.Item2);
+                res.Add((i, (long)average));
+            }
+
+            return res;
+        }
     }
 
     public abstract class IResercheable
@@ -43,15 +93,15 @@ namespace AlgorytmsLibrary
         //value - значение, которое ищем в массиве
         protected IResercheable(int size, string name)
         {
+            TestArray = GenerateArray(size);
             Name = name;
         }
                 
         public abstract void Run(int[] array, int value = 0);
 
         public int[] TestArray { get; }
-        //Имя, которое отображается в экспортном файле
+        
         public string Name { get; }
-    }
-    
+    }  
 }
     
